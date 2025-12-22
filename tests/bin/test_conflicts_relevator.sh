0eaffe40fd745c0651a7365a68057d3459d0bcf3
MarcoGanassin <marcoganassin96@gmail.com>
2025-12-22 19:04:47 +0100

    feat: add Bitbucket provider handling in conflict_relevator + updated conflict_relevator test

--- tests/bin/test_conflicts_relevator.sh
+++ tests/bin/test_conflicts_relevator.sh
@@ -11,6 +11,8 @@ TEST_DIR="$PROJECT_ROOT_DIR/tests"
 
 # Macro
 GITHUB_TEST_REPO_URL="https://github.com/marcoganassin96/git-conflicts-predictor-tester-github.git"
+BITBUCKET_TEST_REPO_URL="https://bitbucket.org/MarcoGanassin/git-conflicts-predictor-tester-bitbucket"
+
 FILES_TO_TEST="README.md,sparkling_water/ai_engine/ai.py"
 declare -A EXPECTED_RESULTS=(
   ["sparkling_water/ai_engine/ai.py"]="feat/improve_sparkling_water_with_ai,2"
@@ -19,22 +21,28 @@ declare -A EXPECTED_RESULTS=(
 
 # Define individual test functions (must start with 'test')
 
-test_relevate_conflicts_api() {
+test_relevate_conflicts_github_api() {
   declare -A relevate_conflicts_result
   manage_conflicts_relevation relevate_conflicts_result --file "$FILES_TO_TEST" --url "$GITHUB_TEST_REPO_URL" --method api --limit 5
   assertArrayEquals EXPECTED_RESULTS relevate_conflicts_result "API method results mismatch"
 }
 
-test_relevate_conflicts_gh() {
+test_relevate_conflicts_github_gh() {
   declare -A relevate_conflicts_result
   manage_conflicts_relevation relevate_conflicts_result --file "$FILES_TO_TEST" --url "$GITHUB_TEST_REPO_URL" --method gh --limit 5
   assertArrayEquals EXPECTED_RESULTS relevate_conflicts_result "GH method results mismatch"
 }
 
-test_relevate_conflicts_auto() {
+test_relevate_conflicts_github_auto() {
   declare -A relevate_conflicts_result
   manage_conflicts_relevation relevate_conflicts_result --file "$FILES_TO_TEST" --url "$GITHUB_TEST_REPO_URL" --limit 5
   assertArrayEquals EXPECTED_RESULTS relevate_conflicts_result "Auto method results mismatch"
+}
+
+test_relevate_conflicts_bitbucket_auto() {
+  declare -A relevate_conflicts_result
+  manage_conflicts_relevation relevate_conflicts_result --file "$FILES_TO_TEST" --url "$BITBUCKET_TEST_REPO_URL" --limit 5
+  assertArrayEquals EXPECTED_RESULTS relevate_conflicts_result "Bitbucket Auto method results mismatch"
 }
 
 # Load shunit2 (This executes the tests)
